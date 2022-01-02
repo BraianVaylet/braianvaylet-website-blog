8dc0b506dd69e2daa3641c7e4314ae8fd6c7a01e
Braian Vaylet <braianvaylet@gmail.com>
2022-01-01 21:44:07 -0300

    working in Navbar & Layout components

--- components/atoms/Icons/LinkedinIcon.tsx
+++
@@ -1,8 +0,0 @@
-import { Icon } from '@chakra-ui/react'
-import { ImLinkedin2 } from 'react-icons/im'
-
-const LinkedinIcon = (props: any) => (
-  <Icon as={ImLinkedin2} viewBox="0 0 200 200" {...props} w={props.fontSize} h={props.fontSize} />
-)
-
-export default LinkedinIcon
