071d02c8a649d6f132a85c1e6668c93d7013850b
Braian Vaylet <braianvaylet@gmail.com>
2022-04-03 12:44:34 -0300

    update colors base and vdark page

--- pages/themes/vdark.tsx
+++ pages/themes/vdark.tsx
@@ -6,142 +6,14 @@ import Layout from 'components/Layout'
 import NextHead from 'components/NextHead'
 import Stepper from 'components/Stepper'
 import AlterLink from 'components/AlterLink'
-import { Logo, Themes } from 'utils/images'
 import { Constants } from 'data/content/constants'
+import { Logo, Themes } from 'utils/images'
+import { ThemeVDark, ThemeVDarkCarrot, ThemeVDarkLemon } from 'utils/constants'
 
 interface ThemeColorProps {
   color: string,
   textColor: string
 }
-
-const ThemeVDark = [
-  {
-    color: '#08090c',
-    textColor: 'white'
-  },
-  {
-    color: '#101219',
-    textColor: 'white'
-  },
-  {
-    color: '#364259',
-    textColor: 'white'
-  },
-  {
-    color: '#7385ab',
-    textColor: 'black'
-  },
-  {
-    color: '#bfc8d9',
-    textColor: 'black'
-  },
-  {
-    color: '#19FDB5',
-    textColor: 'black'
-  },
-  {
-    color: '#18D89B',
-    textColor: 'black'
-  },
-  {
-    color: '#FF6AA3',
-    textColor: 'black'
-  },
-  {
-    color: '#f56565',
-    textColor: 'black'
-  },
-  {
-    color: '#f6e05e',
-    textColor: 'black'
-  }
-]
-
-const ThemeVDarkCarrot = [
-  {
-    color: '#08090c',
-    textColor: 'white'
-  },
-  {
-    color: '#101219',
-    textColor: 'white'
-  },
-  {
-    color: '#364259',
-    textColor: 'white'
-  },
-  {
-    color: '#7385ab',
-    textColor: 'black'
-  },
-  {
-    color: '#bfc8d9',
-    textColor: 'black'
-  },
-  {
-    color: '#ffaa3a',
-    textColor: 'black'
-  },
-  {
-    color: '#ed8936',
-    textColor: 'black'
-  },
-  {
-    color: '#48bb78',
-    textColor: 'black'
-  },
-  {
-    color: '#f56565',
-    textColor: 'black'
-  },
-  {
-    color: '#f6e05e',
-    textColor: 'black'
-  }
-]
-
-const ThemeVDarkLemon = [
-  {
-    color: '#08090c',
-    textColor: 'white'
-  },
-  {
-    color: '#101219',
-    textColor: 'white'
-  },
-  {
-    color: '#364259',
-    textColor: 'white'
-  },
-  {
-    color: '#7385ab',
-    textColor: 'black'
-  },
-  {
-    color: '#bfc8d9',
-    textColor: 'black'
-  },
-  {
-    color: '#d8ff00',
-    textColor: 'black'
-  },
-  {
-    color: '#a6ff00',
-    textColor: 'black'
-  },
-  {
-    color: '#36f213',
-    textColor: 'black'
-  },
-  {
-    color: '#f56565',
-    textColor: 'black'
-  },
-  {
-    color: '#f6e05e',
-    textColor: 'black'
-  }
-]
 
 const ColorBrand = ({ bgColor, textColor }: {bgColor: string, textColor?: string}) => (
   <Flex
---
+++ utils/constants.ts
@@ -0,0 +1,128 @@
+export const ThemeVDark = [
+  {
+    color: '#08090c',
+    textColor: 'white'
+  },
+  {
+    color: '#101219',
+    textColor: 'white'
+  },
+  {
+    color: '#364259',
+    textColor: 'white'
+  },
+  {
+    color: '#7385ab',
+    textColor: 'black'
+  },
+  {
+    color: '#bfc8d9',
+    textColor: 'black'
+  },
+  {
+    color: '#19FDB5',
+    textColor: 'black'
+  },
+  {
+    color: '#18D89B',
+    textColor: 'black'
+  },
+  {
+    color: '#FF6AA3',
+    textColor: 'black'
+  },
+  {
+    color: '#f56565',
+    textColor: 'black'
+  },
+  {
+    color: '#f6e05e',
+    textColor: 'black'
+  }
+]
+
+export const ThemeVDarkCarrot = [
+  {
+    color: '#08090c',
+    textColor: 'white'
+  },
+  {
+    color: '#101219',
+    textColor: 'white'
+  },
+  {
+    color: '#364259',
+    textColor: 'white'
+  },
+  {
+    color: '#7385ab',
+    textColor: 'black'
+  },
+  {
+    color: '#bfc8d9',
+    textColor: 'black'
+  },
+  {
+    color: '#ffaa3a',
+    textColor: 'black'
+  },
+  {
+    color: '#ed8936',
+    textColor: 'black'
+  },
+  {
+    color: '#48bb78',
+    textColor: 'black'
+  },
+  {
+    color: '#f56565',
+    textColor: 'black'
+  },
+  {
+    color: '#f6e05e',
+    textColor: 'black'
+  }
+]
+
+export const ThemeVDarkLemon = [
+  {
+    color: '#08090c',
+    textColor: 'white'
+  },
+  {
+    color: '#101219',
+    textColor: 'white'
+  },
+  {
+    color: '#364259',
+    textColor: 'white'
+  },
+  {
+    color: '#7385ab',
+    textColor: 'black'
+  },
+  {
+    color: '#bfc8d9',
+    textColor: 'black'
+  },
+  {
+    color: '#d8ff00',
+    textColor: 'black'
+  },
+  {
+    color: '#a6ff00',
+    textColor: 'black'
+  },
+  {
+    color: '#36f213',
+    textColor: 'black'
+  },
+  {
+    color: '#f56565',
+    textColor: 'black'
+  },
+  {
+    color: '#f6e05e',
+    textColor: 'black'
+  }
+]
